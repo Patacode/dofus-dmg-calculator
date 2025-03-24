ef730283ab5e0960e8c3099041aa66a8937c266b
Maximilien Ballesteros <maxou.info@gmail.com>
2025-03-24 21:48:51 +0100

    perf: floor estimated damage as u64 instead of i64 for extended value range
    Result can never be negative, so leveraging from that fact to increase available bit range from 32 bits to 64 bits.

--- src/main.rs
+++ src/main.rs
@@ -68,10 +68,10 @@ struct Args {
     show_author: bool,
 }
 
-fn calculate_damage(base_damage: u64, relevant_stat: u64, power: u64, fixed_bonus: u64) -> i64 {
+fn calculate_damage(base_damage: u64, relevant_stat: u64, power: u64, fixed_bonus: u64) -> u64 {
     let multiplier = 1.0 + (relevant_stat + power) as f64 / 100.0;
     let total = base_damage as f64 * multiplier + fixed_bonus as f64;
-    total.floor() as i64
+    total.floor() as u64
 }
 
 fn main() {
