eb9caf7d26765f683334cf875521936bc56c98c6
Maximilien Ballesteros <maxou.info@gmail.com>
2025-03-25 03:19:42 +0100

    refactor: remove unnecessary black box wrapper

--- benches/lib_benchmark.rs
+++ benches/lib_benchmark.rs
@@ -1,16 +1,9 @@
-use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use criterion::{Criterion, criterion_group, criterion_main};
 use dofus_dmg_calculator::compute_dmg_estimation;
 
 fn benchmark_function(c: &mut Criterion) {
     c.bench_function("compute_dmg_estimation", |b| {
-        b.iter(|| {
-            compute_dmg_estimation(
-                black_box(11),
-                black_box(1),
-                black_box(128),
-                black_box(3),
-            )
-        })
+        b.iter(|| compute_dmg_estimation(11, 1, 128, 3))
     });
 }
 
