47552415aaf10f7534fc1ba50c57bb7124b2c015
Maximilien Ballesteros <maxou.info@gmail.com>
2025-03-25 02:42:31 +0100

    test: rename unit test functions

--- src/lib.rs
+++ src/lib.rs
@@ -14,14 +14,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_compute_dmg_estimation_with_zero_values() {
+    fn it_computes_dmg_estimation_with_null_values() {
         let expected = 0;
         let actual = compute_dmg_estimation(0, 0, 0, 0);
         assert_eq!(actual, expected);
     }
 
     #[test]
-    fn test_compute_dmg_estimation_with_positive_values() {
+    fn it_computes_dmg_estimation_with_positive_values() {
         let expected = 56;
         let actual = compute_dmg_estimation(9, 34, 128, 23);
         assert_eq!(actual, expected);
