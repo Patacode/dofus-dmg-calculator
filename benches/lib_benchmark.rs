7517cb138f9279b113b343974c8fe835c79c617c
Patacode <pata.codegineer@gmail.com>
2025-04-02 20:16:33 +0200

    test: add benchmarking for new function

--- benches/lib_benchmark.rs
+++ benches/lib_benchmark.rs
@@ -1,11 +1,22 @@
 use criterion::{Criterion, criterion_group, criterion_main};
 use dofus_dmg_calculator::compute_dmg_estimation;
+use dofus_dmg_calculator::compute_dmg_estimation_with_res;
 
-fn benchmark_function(c: &mut Criterion) {
+fn benchmark_compute_dmg_estimation(c: &mut Criterion) {
     c.bench_function("compute_dmg_estimation", |b| {
         b.iter(|| compute_dmg_estimation(11, 1, 128, 3))
     });
 }
 
-criterion_group!(benches, benchmark_function);
+fn benchmark_compute_dmg_estimation_with_res(c: &mut Criterion) {
+    c.bench_function("compute_dmg_estimation_with_res", |b| {
+        b.iter(|| compute_dmg_estimation_with_res(11, 1, 128, 3, 5, 30))
+    });
+}
+
+criterion_group!(
+    benches,
+    benchmark_compute_dmg_estimation,
+    benchmark_compute_dmg_estimation_with_res
+);
 criterion_main!(benches);
